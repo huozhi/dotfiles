cdab86be861edb045be6306f8424c330c7c54bf1
Jiachi Liu <inbox@huozhi.im>
2016-08-08 01:02:58 +0800

    add vimrc extends from amix/vimrc and node.js install script

---
+++ node/install.sh
@@ -0,0 +1,20 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | zsh
+
+exec zsh
+nvm install v6
+
+npm i -g bower \
+  coffe-script \
+  git-recent \
+  grunt-cli \
+  gulp-cli \
+  diff-so-fancy \
+  http-server \
+  hexo-cli \
+  nodemon \
+  nrm \
+  tldr
+
+
+
+
