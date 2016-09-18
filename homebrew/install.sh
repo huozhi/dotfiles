ea594572425b2969708af66d388bcc5f4d95f1f2
Jiachi Liu <inbox@huozhi.im>
2016-09-19 00:30:09 +0800

    add brewfile, add iterm2 themes, add zsh theme

---
+++ homebrew/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+if test ! $(which brew)
+then
+  echo "  Installing homebrew..."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  fi
+
+fi
+
+exit 0
